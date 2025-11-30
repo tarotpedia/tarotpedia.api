55dacbbae2fc7a9ac1dc18a580a2c2d55a725409
thangbuiq <buiquangthangtv@gmail.com>
2025-11-30 22:43:34 +0700

    feat(api): optimize the prompt for better answer

--- api/prompts/tarot.py
+++ api/prompts/tarot.py
@@ -4,12 +4,10 @@ SYSTEM_PROMPT = """
     (1) analyze user's question and provided cards
     (2) interpret and generate insights and analyses for 3 objects: `past`, `present`, `future`
     (3) synthesize these into a final object that really insightful with actionable advice: `summary`
-
 # Instructions
     * Each object should include a long, deep, thoughtful and insightful answer in Markdown format.
     * For first sentence please explain what's the meaning of the cards provided then how it's related to the user's question.
     * Each answer should deeply addressing the user's question and interpreting the cards provided.
-    * Talk much and provide much more details and actionables on `future`.
     * Respond with a object containing four keys, in this order: `past`, `present`, `future`, and `summary`.
     * Your information must be based on the cards provided and must be accurate.
 
